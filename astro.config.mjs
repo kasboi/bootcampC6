ccd9fbb4f57edb0852142e51da7f39c18a1ab2fb
Abdullah <solahudeen15@gmail.com>
2025-04-08 11:44:36 +0100

    prettier config, writeups

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,49 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          slug: "intro",
+        },
+        {
+          label: "Motivation",
+          slug: "motivation",
+        },
+        {
+          label: "Code Editor Setup",
+          slug: "code-editor",
+        },
+        {
+          label: "HTML Fundamentals",
+          items: [
+            {
+              label: "HTML 101",
+              slug: "html/html-101",
+            },
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
