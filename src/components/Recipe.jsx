07c01fc87749f1099feebdd7a6280ef1ec991bf7
Abdullah <solahudeen15@gmail.com>
2025-04-28 10:40:29 +0100

    css recipe project

---
+++ src/components/Recipe.jsx
@@ -0,0 +1,26 @@
+import styles from "./Codepen.module.css";
+
+export default function Recipe() {
+  return (
+    <iframe
+      style={{ width: "100%", height: "600px" }}
+      scrolling="no"
+      title="FM - recipe"
+      src="https://codepen.io/kolawole-abdullah-solahudeen/embed/MYYvLeG?default-tab=result"
+      frameborder="no"
+      loading="lazy"
+      allowtransparency="true"
+      allowfullscreen="true"
+    >
+      See the Pen{" "}
+      <a href="https://codepen.io/kolawole-abdullah-solahudeen/pen/MYYvLeG">
+        FM - recipe
+      </a>{" "}
+      by Kolawole Abdullah Solahudeen (
+      <a href="https://codepen.io/kolawole-abdullah-solahudeen">
+        @kolawole-abdullah-solahudeen
+      </a>
+      ) on <a href="https://codepen.io">CodePen</a>.
+    </iframe>
+  );
+}
